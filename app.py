386e829d1cde7f6ab7a7e1b73a784f03af33ed67
discdiver <jeffmshale@gmail.com>
2021-03-31 18:34:20 -0400

    working containers

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ import altair
 import seaborn as sns
 import plotly.express as px
 
-
-col1, col2, col3 = st.beta_columns(3)
-
+st.set_page_config(layout="wide")
 
 plot_types = ("histogram", "bar", "boxplot", "scatter", "line")
 
@@ -16,18 +14,15 @@ plot_types = ("histogram", "bar", "boxplot", "scatter", "line")
 np.random.seed(34)
 df = pd.DataFrame(np.random.randn(100, 2), columns=["Var1", "Var2"])
 
-st.title("Data Viz Gallery")
-st.subheader(
-    "A gallery of data viz with Python code for common libraries and common chart types"
-)
-
 # sidebar
 chart_type = st.sidebar.selectbox("Chart type", plot_types)
 
-"Showing:", chart_type
+with st.beta_container():
+    # plot
+    st.header(f"Showing:  {chart_type}")
 
+col1, col2, col3 = st.beta_columns(3)
 
-# plot
 with col1:
     st.header("Scatterplot")
     "Matplotlib"
@@ -42,7 +37,7 @@ with col1:
         st.pyplot(fig)
 
     with st.echo():
-        fig = px.scatter(df, x="Var1", y="Var2", title=f"Plotly Express {chart_type}")
+        fig = px.scatter(df, x="Var1", y="Var2")
         st.plotly_chart(fig, use_container_width=True)
 
 with col2:
@@ -54,8 +49,14 @@ with col2:
         ax.hist(df["Var1"])
         st.pyplot(fig)
 
+    "Seaborn"
     with st.echo():
-        fig = px.histogram(df, "Var1", title=f"Plotly Express {chart_type}")
+        sns.histplot(data=df, x="Var1")
+        st.pyplot(fig)
+
+    "Plotly Express"
+    with st.echo():
+        fig = px.histogram(df, "Var1")
         st.plotly_chart(fig, use_container_width=True)
 
 with col3:
@@ -63,24 +64,26 @@ with col3:
     "Matplotlib"
     with st.echo():
         fig, ax = plt.subplots()
-        ax.scatter(x=df["Var1"], y=df["Var2"])
+        ax.bar(x=df["Var1"], height=df["Var2"])
         st.pyplot(fig)
 
-    st.write("Seaborn")
+    "Seaborn"
     with st.echo():
-        sns.scatterplot(data=df, x="Var1", y="Var2")
+        sns.barplot(data=df, x="Var1", y="Var2")
         st.pyplot(fig)
 
+    "Plotly Express"
+    
     with st.echo():
-        fig = px.scatter(df, x="Var1", y="Var2", title=f"Plotly Express {chart_type}")
+        fig = px.bar(df, "Var1", "Var2")
         st.plotly_chart(fig, use_container_width=True)
 
+with st.beta_container():
+    show_data = st.checkbox("See the raw data?")
+    if show_data:
+        df
 
-show_data = st.checkbox("See the raw data?")
-if show_data:
-    df
-
-st.text(
-    """Python has many data visualization libraries. This gallery is not exhaustive. 
-If you would like to add code for another library, please submit a pull request."""
-)
+    st.text(
+        """Python has many data visualization libraries. This gallery is not exhaustive. 
+    If you would like to add code for another library, please submit a pull request."""
+    )
