e0b72d6e2a6e85b9ea19de943504515cc55f1c21
discdiver <jeffmshale@gmail.com>
2021-03-31 18:58:59 -0400

    one column, choose plot

--- app.py
+++ app.py
@@ -8,75 +8,77 @@ import plotly.express as px
 
 st.set_page_config(layout="wide")
 
-plot_types = ("histogram", "bar", "boxplot", "scatter", "line")
+plot_types = ("Histogram", "Bar", "Boxplot", "Scatter", "Line")
 
 # TODO change for reproducibilty and use caching
 np.random.seed(34)
 df = pd.DataFrame(np.random.randn(100, 2), columns=["Var1", "Var2"])
 
-# sidebar
-chart_type = st.sidebar.selectbox("Chart type", plot_types)
+# sidebar to get user type
+chart_type = st.selectbox("Choose your chart type", plot_types)
 
 with st.beta_container():
     # plot
     st.header(f"Showing:  {chart_type}")
 
-col1, col2, col3 = st.beta_columns(3)
+col1, col2 = st.beta_columns(2)
+
+
+def return_matplotlib_plot(plot_type: str):
+    """ return matplotlib plots """
+
+    fig, ax = plt.subplots()
+    if chart_type == "Scatter":
+        ax.scatter(x=df["Var1"], y=df["Var2"])
+    elif chart_type == "Histogram":
+        ax.hist(df["Var1"])
+    elif chart_type == "Bar":
+        ax.bar(x=df["Var1"], height=df["Var2"])
+    return fig
+
+
+def return_sns_plot(plot_type: str):
+    """ return seaborn plots """
+
+    fig, ax = plt.subplots()
+    if chart_type == "Scatter":
+        sns.scatterplot(data=df, x="Var1", y="Var2")
+    elif chart_type == "Histogram":
+        sns.histplot(data=df, x="Var1")
+    elif chart_type == "Bar":
+        sns.barplot(data=df, x="Var1", y="Var2")
+    return fig
+
+
+def return_plotly_plot(plot_type: str):
+    """ return plotly plots """
+
+    if chart_type == "Scatter":
+        fig = px.scatter(df, x="Var1", y="Var2")
+    elif chart_type == "Histogram":
+        fig = px.histogram(df, "Var1")
+    elif chart_type == "Bar":
+        fig = px.bar(df, "Var1", "Var2")
+    return fig
+
 
 with col1:
-    st.header("Scatterplot")
-    "Matplotlib"
-    with st.echo():
-        fig, ax = plt.subplots()
-        ax.scatter(x=df["Var1"], y=df["Var2"])
-        st.pyplot(fig)
-
-    st.write("Seaborn")
-    with st.echo():
-        sns.scatterplot(data=df, x="Var1", y="Var2")
-        st.pyplot(fig)
-
-    with st.echo():
-        fig = px.scatter(df, x="Var1", y="Var2")
-        st.plotly_chart(fig, use_container_width=True)
-
-with col2:
-    st.header("Histogram")
 
     "Matplotlib"
     with st.echo():
-        fig, ax = plt.subplots()
-        ax.hist(df["Var1"])
-        st.pyplot(fig)
+        plot = return_matplotlib_plot(chart_type)
+        st.pyplot(plot)
 
     "Seaborn"
     with st.echo():
-        sns.histplot(data=df, x="Var1")
-        st.pyplot(fig)
+        plot = return_sns_plot(chart_type)
+        st.pyplot(plot)
 
     "Plotly Express"
     with st.echo():
-        fig = px.histogram(df, "Var1")
-        st.plotly_chart(fig, use_container_width=True)
+        plot = return_plotly_plot(chart_type)
+        st.plotly_chart(plot, use_container_width=True)
 
-with col3:
-    st.header("Bar chart")
-    "Matplotlib"
-    with st.echo():
-        fig, ax = plt.subplots()
-        ax.bar(x=df["Var1"], height=df["Var2"])
-        st.pyplot(fig)
-
-    "Seaborn"
-    with st.echo():
-        sns.barplot(data=df, x="Var1", y="Var2")
-        st.pyplot(fig)
-
-    "Plotly Express"
-    
-    with st.echo():
-        fig = px.bar(df, "Var1", "Var2")
-        st.plotly_chart(fig, use_container_width=True)
 
 with st.beta_container():
     show_data = st.checkbox("See the raw data?")
