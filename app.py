3cdcc3ae1dddbd44ff9b0627b9eedd613bfae582
discdiver <jeffmshale@gmail.com>
2021-04-02 20:10:48 -0400

    can switch to 3 columns with penguins

--- app.py
+++ app.py
@@ -6,13 +6,18 @@ import seaborn as sns
 import plotly.express as px
 import altair as alt
 
+
 st.set_page_config(layout="wide")
 
 plot_types = ("Histogram", "Bar", "Boxplot", "Scatter", "Line")
 
-# TODO change for reproducibilty and use caching
-np.random.seed(34)
-df = pd.DataFrame(np.random.randn(100, 2), columns=["Var1", "Var2"])
+@st.cache
+def load_penguins():
+    return sns.load_dataset("penguins")
+
+
+df = load_penguins()
+
 
 # sidebar to get user type
 chart_type = st.selectbox("Choose your chart type", plot_types)
@@ -21,9 +26,12 @@ with st.beta_container():
     # plot
     st.header(f"Showing:  {chart_type}")
 
-   
 
-# col1, co2 = st.beta_columns(2)
+three_cols = st.checkbox("3 columns?")
+if three_cols:
+    col1, col2, col3 = st.beta_columns(3)
+else:
+    col1 = st.beta_columns(1)
 
 
 def return_matplotlib_plot(plot_type: str):
@@ -31,15 +39,15 @@ def return_matplotlib_plot(plot_type: str):
 
     fig, ax = plt.subplots()
     if chart_type == "Scatter":
-        ax.scatter(x=df["Var1"], y=df["Var2"])
+        ax.scatter(x=df["bill_depth_mm"], y=df["bill_length_mm"])
     elif chart_type == "Histogram":
-        ax.hist(df["Var1"])
+        ax.hist(df["bill_depth_mm"])
     elif chart_type == "Bar":
-        ax.bar(x=df["Var1"], height=df["Var2"])
+        ax.bar(x=df["bill_depth_mm"], height=df["bill_length_mm"])
     elif chart_type == "Boxplot":
-        ax.boxplot(x=df["Var1"])
+        ax.boxplot(x=df["bill_depth_mm"])
     elif chart_type == "Line":
-        ax.plt(x=df["Var1"], y=df["Var2"])
+        ax.plot(df["bill_depth_mm"], df["bill_length_mm"])
     return fig
 
 
@@ -48,73 +56,107 @@ def return_sns_plot(plot_type: str):
 
     fig, ax = plt.subplots()
     if chart_type == "Scatter":
-        sns.scatterplot(data=df, x="Var1", y="Var2")
+        sns.scatterplot(data=df, x="bill_depth_mm", y="bill_length_mm")
     elif chart_type == "Histogram":
-        sns.histplot(data=df, x="Var1")
+        sns.histplot(data=df, x="bill_depth_mm")
     elif chart_type == "Bar":
-        sns.barplot(data=df, x="Var1", y="Var2")
+        sns.barplot(data=df, x="bill_depth_mm", y="bill_length_mm")
     elif chart_type == "Boxplot":
-        sns.boxplot(data=df, x="Var1")
+        sns.boxplot(data=df, x="bill_depth_mm")
     elif chart_type == "Line":
-        sns.lineplot(data=df, x="Var1", y="Var2")
+        sns.lineplot(data=df, x="bill_depth_mm", y="bill_length_mm")
     return fig
 
-@st.cache
+
+@st.cache  # only one that doesn't give a warning if cache it
 def return_plotly_plot(plot_type: str):
     """ return plotly plots """
 
     if chart_type == "Scatter":
-        fig = px.scatter(df, x="Var1", y="Var2")
+        fig = px.scatter(df, x="bill_depth_mm", y="bill_length_mm")
     elif chart_type == "Histogram":
-        fig = px.histogram(df, "Var1")
+        fig = px.histogram(df, "bill_depth_mm")
     elif chart_type == "Bar":
-        fig = px.bar(df, "Var1", "Var2")
+        fig = px.bar(df, "bill_depth_mm", "bill_length_mm")
     elif chart_type == "Boxplot":
-        fig = px.box(df, "Var1")
+        fig = px.box(df, "bill_depth_mm")
     elif chart_type == "Line":
-        fig = px.line(df, "Var1", "Var2")
+        fig = px.line(df, "bill_depth_mm", "bill_length_mm")
     return fig
 
-@st.cache
+
 def return_altair_plot(plot_type: str):
     """ return altair plots """
 
     if chart_type == "Scatter":
-        fig = alt.Chart(df).mark_bar().encode(x='Var1', y='Var2')
+        fig = alt.Chart(df).mark_bar().encode(x="bill_depth_mm", y="bill_length_mm")
     elif chart_type == "Histogram":
-        fig = alt.Chart(df).mark_bar().encode(x='Var1', y='Var2')
+        fig = alt.Chart(df).mark_bar().encode(x="bill_depth_mm", y="bill_length_mm")
     elif chart_type == "Bar":
-        fig = alt.Chart(df).mark_bar().encode(x='Var1', y='Var2')
+        fig = alt.Chart(df).mark_bar().encode(x="bill_depth_mm", y="bill_length_mm")
     elif chart_type == "Boxplot":
-        fig = alt.Chart(df).mark_bar().encode(x='Var1', y='Var2')
+        fig = alt.Chart(df).mark_bar().encode(x="bill_depth_mm", y="bill_length_mm")
     elif chart_type == "Line":
-        fig = alt.Chart(df).mark_bar().encode(x='Var1', y='Var2')
+        fig = alt.Chart(df).mark_bar().encode(x="bill_depth_mm", y="bill_length_mm")
     return fig
 
 
+# functions to output data
+
+
+def show_plt():
+    st.write("Matplotlib")
+    with st.echo():
+        plot = return_matplotlib_plot(chart_type)
+        st.pyplot(plot)
+
+
+def show_seaborn():
+    st.write("Seaborn")
+    with st.echo():
+        plot = return_sns_plot(chart_type)
+        st.pyplot(plot)
+
+
+def show_plotly():
+    st.write("Plotly Express")
+    with st.echo():
+        plot = return_plotly_plot(chart_type)
+        st.plotly_chart(plot, use_container_width=True)
+
+
+def show_altair():
+    st.write("Altair")
+    with st.echo():
+        plot = return_altair_plot(chart_type)
+        st.altair_chart(plot, use_container_width=True)
+
+
 # output
-# with col1:
+if three_cols:
+    with col1:
+        show_plt()
+else:
+    show_plt()
 
-"Matplotlib"
-with st.echo():
-    plot = return_matplotlib_plot(chart_type)
-    st.pyplot(plot)
+if three_cols:
+    with col2:
+        show_seaborn()
+else:
+    show_seaborn()
 
-"Seaborn"
-with st.echo():
-    plot = return_sns_plot(chart_type)
-    st.pyplot(plot)
+if three_cols:
+    with col3:
+        show_plotly()
+else:
+    show_plotly()
 
-"Plotly Express"
-with st.echo():
-    plot = return_plotly_plot(chart_type)
-    st.plotly_chart(plot, use_container_width=True)
+if three_cols:
+    with col1:
+        show_altair()
+else:
+    show_altair()
 
-# saying cache mutation issue
-# "Altair"
-# with st.echo():
-#     plot = return_altair_plot(chart_type)
-#     st.altair_chart(plot, use_container_width=True)
 
 with st.beta_container():
     show_data = st.checkbox("See the raw data?")
